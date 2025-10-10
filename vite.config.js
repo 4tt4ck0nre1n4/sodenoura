ccdf3ae93f8d824d9d5356bb3b9a31daf1814927
hibari <webengineer@hibari-konaweb.com>
2025-10-10 14:29:40 +0900

    fix: change Vite base path to absolute and improve middleware static asset handling

--- vite.config.js
+++ vite.config.js
@@ -26,7 +26,7 @@ export default defineConfig(async () => {
 
   return {
     root: root,
-    base: "./",
+    base: "/",
     publicDir: resolve(__dirname, "public"),
     server: {
       port: 8080,
